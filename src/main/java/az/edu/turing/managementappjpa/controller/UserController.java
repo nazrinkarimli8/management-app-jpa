cdcca5251a719e88b13ca8db834296cf664a3235
nazrinkarimli8 <karimlinazrin8@gmail.com>
2024-07-29 15:42:28 +0400

    Add logger

--- src/main/java/az/edu/turing/managementappjpa/controller/UserController.java
+++ src/main/java/az/edu/turing/managementappjpa/controller/UserController.java
@@ -4,6 +4,8 @@ import az.edu.turing.managementappjpa.domain.entity.UserEntity;
 import az.edu.turing.managementappjpa.model.UserDto;
 import az.edu.turing.managementappjpa.service.impl.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,46 +16,54 @@ import java.util.Optional;
 @RequestMapping("/api/v1/users")
 public class UserController {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
     private final UserServiceImpl userService;
-    private final UserServiceImpl userServiceImpl;
 
     @PostMapping
     public void createUser(@RequestBody UserDto userDto) {
+        logger.info("Create user request received: {}", userDto);
         userService.save(userDto);
     }
 
     @GetMapping("/all")
     public List<UserEntity> getAllUsers() {
+        logger.info("Get all users request received");
         return userService.getAll();
     }
 
     @GetMapping("/{id}")
     public Optional<UserEntity> getUserById(@PathVariable Long id) {
+        logger.info("Get user by id request received: {}", id);
         return userService.getById(id);
     }
 
     @DeleteMapping("/all")
     public void deleteAllUsers() {
+        logger.info("Delete all users request received");
         userService.deleteAll();
     }
 
     @DeleteMapping("/{id}")
     public void deleteUserById(@PathVariable Long id) {
+        logger.info("Delete user by id request received: {}", id);
         userService.deleteById(id);
     }
 
     @PutMapping("/{id}")
     public UserEntity updateUserById(@PathVariable Long id, @RequestBody UserEntity userEntity) {
+        logger.info("Update user by id request received: {}", id);
         return userService.update(id, userEntity);
     }
 
     @PatchMapping("/age/{id}")
     public UserEntity updateUserAge(@PathVariable Long id, @RequestBody UserEntity userEntity) {
-        return userServiceImpl.updateAge(id, userEntity);
+        logger.info("Update user age by id request received: {}", id);
+        return userService.updateAge(id, userEntity);
     }
 
     @GetMapping("/count")
     public long getCountUsers() {
+        logger.info("Get count of users request received");
         return userService.countUsers();
     }
 }
--- src/main/java/az/edu/turing/managementappjpa/service/impl/UserServiceImpl.java
+++ src/main/java/az/edu/turing/managementappjpa/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package az.edu.turing.managementappjpa.service.impl;
 
 import az.edu.turing.managementappjpa.domain.entity.UserEntity;
 import az.edu.turing.managementappjpa.domain.repository.UserRepository;
+import az.edu.turing.managementappjpa.exception.UserNotFoundException;
 import az.edu.turing.managementappjpa.model.UserDto;
 import az.edu.turing.managementappjpa.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -18,7 +19,6 @@ import java.util.Optional;
 public class UserServiceImpl implements UserService {
 
     private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
-
     private final UserRepository userRepository;
 
     @Override
@@ -43,17 +43,26 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Optional<UserEntity> getById(Long id) {
-        return userRepository.findById(id);
+        Optional<UserEntity> user = userRepository.findById(id);
+        if (user.isPresent()) {
+            logger.info("User found with id: {}", id);
+        } else {
+            logger.warn("User not found with id: {}", id);
+            throw new UserNotFoundException("User not found with id: " + id);
+        }
+        return user;
     }
 
     @Override
     public void deleteAll() {
         userRepository.deleteAll();
+        logger.info("All users deleted");
     }
 
     @Override
     public void deleteById(Long id) {
         userRepository.deleteById(id);
+        logger.info("User deleted with id: {}", id);
     }
 
     @Override
@@ -68,9 +77,12 @@ public class UserServiceImpl implements UserService {
             updatedUser.setUpdated(LocalDateTime.now());
             updatedUser.setProfilePhoto(userEntity.getProfilePhoto());
             userRepository.save(updatedUser);
+            logger.info("User updated with id: {}", id);
             return updatedUser;
+        } else {
+            logger.warn("User not found with id: {}", id);
+            throw new UserNotFoundException("User not found with id: " + id);
         }
-        return null;
     }
 
     @Override
@@ -80,13 +92,18 @@ public class UserServiceImpl implements UserService {
             UserEntity updatedUser = foundedUser.get();
             updatedUser.setAge(userEntity.getAge());
             userRepository.save(updatedUser);
+            logger.info("User age updated with id: {}", id);
             return updatedUser;
+        } else {
+            logger.warn("User not found with id: {}", id);
+            throw new UserNotFoundException("User not found with id: " + id);
         }
-        return null;
     }
 
     @Override
     public long countUsers() {
-        return userRepository.count();
+        long count = userRepository.count();
+        logger.info("Total number of users: {}", count);
+        return count;
     }
 }
